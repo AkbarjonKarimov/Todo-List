ac16771c56dfa24ac28e38583e26a3a025a00cfb
Karimov Akbar <karimovakbar2000@gmail.com>
2021-04-12 08:44:48 +0500

    Laravel project

---
+++ resources/js/vue/addItemForm.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="addItem">
+        <input type="text" v-model="item.name" />
+        <font-awesome-icon
+            icon = "plus-square"
+            @click="addItem()"
+            :class=" [item.name ? 'active' : 'incative','plus'] "
+        />
+    </div>
+</template>
+
+<script>
+    export default{
+        data:function(){
+            return{
+                item: {
+                    name:""
+                }
+            }
+        },
+        methods: {
+            addItem(){
+                if(this.item.name ==''){
+                    return;
+                }
+                axios.post('api/item/store',{
+                    item:this.item
+                })
+                .then(response=>{
+                    if(response.status==201){
+                        this.item.name=="";
+                        this.$emit('reloadlist');
+                    }
+                })
+                .catch(error=>{
+                    console.log(error);
+                })
+            }
+        } 
+    }
+</script>
+<style scoped>
+.addItem{
+    display: flex;
+    justify-content:center;
+    align-items: center;
+}
+input{
+    background: #f7f7f7;
+    border:0px;
+    outline: none;
+    padding: 5px;
+    margin-right: 10px;
+    width: 100%;
+}
+.plus{
+    font-size:20px;
+}
+.active{
+    color:#00CE25;
+}
+.inactive{
+    color:#999999;
+}
+</style>
